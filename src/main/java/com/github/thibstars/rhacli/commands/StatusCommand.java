7091e3218a35ee3af7888df4dbc7eb2cae714b3b
Thibstars <thibault.helsmoortel@gmail.com>
2024-07-12 19:55:51 +0200

    Make base url configurable

--- src/main/java/com/github/thibstars/rhacli/commands/StatusCommand.java
+++ src/main/java/com/github/thibstars/rhacli/commands/StatusCommand.java
@@ -4,6 +4,8 @@ import com.github.thibstars.jhaapi.Configuration;
 import com.github.thibstars.jhaapi.client.status.Status;
 import com.github.thibstars.jhaapi.client.status.StatusService;
 import com.github.thibstars.jhaapi.client.status.StatusServiceImpl;
+import java.net.MalformedURLException;
+import java.net.URI;
 import picocli.CommandLine.Command;
 
 /**
@@ -15,7 +17,17 @@ public class StatusCommand extends SubCommand {
 
     @Override
     public void run() {
-        Configuration configuration = new Configuration(token);
+        Configuration configuration;
+        if (baseUrl == null) {
+            configuration = new Configuration(token);
+        } else {
+            try {
+                configuration = new Configuration(URI.create(baseUrl).toURL(), token);
+            } catch (MalformedURLException | IllegalArgumentException e) {
+                System.out.println("Malformed URL: " + baseUrl + ", will use default configuration instead.");
+                configuration = new Configuration(token);
+            }
+        }
         StatusService statusService = new StatusServiceImpl(configuration);
 
         System.out.println(
--- src/main/java/com/github/thibstars/rhacli/commands/SubCommand.java
+++ src/main/java/com/github/thibstars/rhacli/commands/SubCommand.java
@@ -9,4 +9,7 @@ public abstract class SubCommand implements Runnable {
 
     @Option(names = {"-t", "--token"}, description = "Long-Lived Access Token to access Home Assistant")
     protected String token;
+
+    @Option(names = {"-u", "--url"}, description = "Base URL of the Home Assistant API")
+    protected String baseUrl;
 }
